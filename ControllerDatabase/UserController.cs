9acfba6bf61045c20bcd054bd0a9f8fb04289c40
Fernando6446 <fmoralesjuarez@gmail.com>
2022-10-26 12:29:17 -0600

    creating user controller

---
+++ ControllerDatabase/UserController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ControllerDatabase
+{
+    public class UserController
+    {
+        public SqlConnection connection;
+        public UserController()
+        {
+        }
+    }
+}

1e49ff28f04c27ed835841413cb2c73c99ddf11e
Fernando6446 <fmoralesjuarez@gmail.com>
2022-10-26 12:42:04 -0600

    creating two methods for create and search user

--- ControllerDatabase/UserController.cs
+++ ControllerDatabase/UserController.cs
@@ -12,6 +12,45 @@ namespace ControllerDatabase
         public SqlConnection connection;
         public UserController()
         {
+            this.connection = DBConnection.getConnection();
+        }
+
+        public bool userExist(String user, String pass)
+        {
+            this.connection.Open();
+            String sql = "select * from Usuario where usuario='" + user + "' and pass='" + pass + "'";
+            SqlCommand sqlCommand = new SqlCommand(sql);
+            SqlDataReader reader = sqlCommand.ExecuteReader();
+            if (reader.Read())
+            {
+                this.connection.Close();
+                return true;
+            }
+            this.connection.Close();
+            return false;
+        }
+
+        public void createUser(String user, String email,String phone, String pass)
+        {
+            this.connection.Open();
+            String sql = "insert into Usuario(usuario, correo, tel, pass)values('" + user + "', '"+email+"', '"+phone+"', '"+pass+"')";
+            SqlCommand cmd = new SqlCommand(sql);
+            cmd.ExecuteNonQuery();
+            this.connection.Close();
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+

365d471c4981b85a15322e213fab768b5f76a235
Fernando6446 <fmoralesjuarez@gmail.com>
2022-10-27 09:22:53 -0600

    receive data in singer item gallery

--- ProyectoDB/SingerItem.cs
+++ ProyectoDB/SingerItem.cs
@@ -12,9 +12,35 @@ namespace ProyectoDB
 {
     public partial class SingerItem : UserControl
     {
-        public SingerItem()
+        public SingerItem(string name)
         {
+            lbl_name.Text = name;
             InitializeComponent();
+        }
+        private void SingerItem_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.BorderStyle = BorderStyle.FixedSingle;
+            this.BackColor = SystemColors.ControlDark;
+        }
+
+        private void SingerItem_MouseLeave(object sender, EventArgs e)
+        {
+            this.BorderStyle = BorderStyle.None;
+            this.BackColor = SystemColors.ControlDarkDark;
+        }
+
+        private void SingerItem_MouseEnter(object sender, EventArgs e)
+        {
+            this.BorderStyle = BorderStyle.FixedSingle;
+            this.BackColor = SystemColors.ControlDark;
+
+        }
+
+
+        public void setImage()
+        {
+            this.BackgroundImage = Image.FromFile("./imgs/Generos/" + lbl_name.Text + ".jpg");
+                this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
         }
     }
 }

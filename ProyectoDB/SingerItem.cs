480d1bc25ad6c2def32faf227efd484a3404617f
Fernando6446 <fmoralesjuarez@gmail.com>
2022-10-27 13:02:56 -0600

    showing singers

--- ProyectoDB/SingerItem.cs
+++ ProyectoDB/SingerItem.cs
@@ -12,33 +12,16 @@ namespace ProyectoDB
 {
     public partial class SingerItem : UserControl
     {
+        public string name;
         public SingerItem(string name)
         {
-            lbl_name.Text = name;
+            this.name = name;
             InitializeComponent();
+            this.setImage();
         }
-        private void SingerItem_MouseDown(object sender, MouseEventArgs e)
-        {
-            this.BorderStyle = BorderStyle.FixedSingle;
-            this.BackColor = SystemColors.ControlDark;
-        }
-
-        private void SingerItem_MouseLeave(object sender, EventArgs e)
-        {
-            this.BorderStyle = BorderStyle.None;
-            this.BackColor = SystemColors.ControlDarkDark;
-        }
-
-        private void SingerItem_MouseEnter(object sender, EventArgs e)
-        {
-            this.BorderStyle = BorderStyle.FixedSingle;
-            this.BackColor = SystemColors.ControlDark;
-        }
-
-
         public void setImage()
         {
-            this.BackgroundImage = Image.FromFile("./imgs/Cantantes/" + lbl_name.Text + ".jpg");
+            this.BackgroundImage = Image.FromFile("./imgs/Cantantes/" + name + ".jpg");
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
         }
     }
